db34e5615d8cfed0083dc51ca1ec0b5d4cf3c250
Jake Field <jacobprogrammes@gmail.com>
2024-03-01 11:54:49 +0000

    findIndex

--- Step 3/step3.js
+++ Step 3/step3.js
@@ -23,13 +23,37 @@
 // 2) currentValue: The current element being processed
 // 3) Index: The index of the current element
 // 4) Array: The array being traversed
-const numbers = [1, 2, 3, 4, 5];
+const numbers1 = [1, 2, 3, 4, 5];
 // reduce
-const sum = numbers.reduce(
+const sum = numbers1.reduce(
   (accumulator, currentValue) => accumulator + currentValue,
   0 // this '0' is the inital value for the accumulator
 );
-console.log(sum);
+console.log(`Sum = ${sum}`);
 
 // reduce takes in up to four parameters, and reduces an array into a single element. 1) Accumulator 2) currentValue 3) Index 4) Array. The result of the above is 15.
 //
+
+//FIND:
+// the 'find' method retrieve the first element in an array that satisfies a given condition.
+// Returns 'undefined' if not gound
+
+const users = [
+  { id: 1, name: "Alice" },
+  { id: 2, name: "Jak" },
+  { id: 3, name: "Bobbie" },
+];
+const user1 = users.find((user1) => user1.name === "Alice");
+const user2 = users.find((user2) => user2.id === 2);
+const user3 = users.find((user3) => user3.id === 3);
+console.log(user1);
+console.log(user2);
+console.log(user3);
+
+// FindIndex
+// findIndex is used to search arrays. It returns the index of the first element that satisfies the given condition; if none is found, it returns -1.
+//Use cases: Finding the position of an item & validating array elements
+const numbers = [10, 20, 30, 40, 50];
+const index = numbers.findIndex((num) => num > 25);
+console.log(`Index = ${index}`);
+// array.findIndex(())
