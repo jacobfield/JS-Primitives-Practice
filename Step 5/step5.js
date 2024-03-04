0a5569d05067fc2384b0785886df83f708411195
Jake Field <jacobprogrammes@gmail.com>
2024-03-04 11:53:34 +0000

    HOF

--- Step 5/step5.js
+++ Step 5/step5.js
@@ -8,3 +8,17 @@
 // Explain the concept of immutability and its importance in functional programming.
 // How do higher-order functions differ from regular functions?
 // Provide an example of using map to transform an array of objects.
+
+// IMMUTABILITY
+//Immutability is a key principle in functional programming that emphasises the importance of not modifying data once it has been created. Once defined, variables cannot be changed. Instead of modifying original ones, create new ones.
+//Importance of Immutability:
+// 1) Predictability - immutable data leads to more predictable code, since the values don't unexpectedly change
+// 2)  Concurrency - Immutability simplifies handling concurrent operations, as there's no risk of data changing whilst it is being processed
+// 3) Debugging - Debugging becomes easier since the state of data remains constant throughout the program
+
+// HIGHER ORDER FUNCTIONS VS. REGULAR FUNCTIONS:
+//HOF: Functions that take one or more functions as arguments, or return a function
+// EG:
+const multiplyBy = (factor) => (num) => num * factor;
+const double = multiplyBy(2);
+console.log(double(5));
