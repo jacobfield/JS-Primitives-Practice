7fe36aae7b91d21d563ed013d595e4d3a83bb791
Jake Field <jacobprogrammes@gmail.com>
2024-03-05 14:09:41 +0000

    step 6 init

---
+++ Step 6/step6.js
@@ -0,0 +1,12 @@
+// Step 6: Practical Applications and Code Challenges
+// Objective: Apply array methods in real-world scenarios and solve coding challenges.
+// Activities:
+// Assign coding projects or tasks that require the use of various array methods.
+// Provide coding challenges focusing on specific array operations and algorithms.
+// Conduct code reviews to evaluate implementation, efficiency, and adherence to best practices.
+// Criteria for Mastery:
+// Successfully complete coding projects/tasks using array methods.
+// Solve coding challenges involving array manipulation efficiently.
+// Receive positive feedback in code reviews regarding implementation and best practices.
+
+const arrToReverse = ["first", "second"];
